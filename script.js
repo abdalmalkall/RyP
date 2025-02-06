b3a2f276f35dc3e4fb225007a9d72de2e3bf09a1
put your github username here <YOUR EMAIL ADDRESS>
2025-02-06 15:25:45 +0300

    mose

--- script.js
+++ script.js
@@ -121,3 +121,11 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("No design name found in the URL.");
   }
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.getElementById("custom-cursor");
+
+  document.addEventListener("mousemove", (event) => {
+      cursor.style.left = event.pageX + "px";
+      cursor.style.top = event.pageY + "px";
+  });
+});
